d7b108be28e4aaa3c8ac94a5a449b247ba9ffff7
Zsoltesz31 <57670791+Zsoltesz31@users.noreply.github.com>
2020-03-27 12:23:41 +0100

    Add files via upload

---
+++ konyvbolt/protected/header.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-4">Üdvözöllek az e-könyvtáromban!</h1>
+    <p class="lead">Vágjunk is bele! Bővebb hozzáféréshez kérlek jelentkezz be, vagy ha még nincs fiókod regisztrálj!</p>
+  </div>
+</div>
