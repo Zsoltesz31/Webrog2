569f612ed2f995ccc9fcc5c5a5228dca2b7a03c1
Zsoltesz31 <57670791+Zsoltesz31@users.noreply.github.com>
2020-04-29 14:04:59 +0200

    Add files via upload
    Regiszráció elkészítsée

--- konyvbolt/protected/nav.php
+++ konyvbolt/protected/nav.php
@@ -6,10 +6,12 @@
   <li class="nav-item">
     <a class="nav-link" data-toggle="modal" href="#exampleModal">Bejelentkezés</a>
 
-<!-- Modal -->
+<?php if(!IsUserLoggedIn()) : ?>
+
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
+    <form method="POST">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Bejelentkezés</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -17,22 +19,33 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
+      <div class="form-row">
+		<div class="form-group col-md-6">
+			<label for="registerPassword">Felhasználónév</label>
+			<input type="text" class="form-control" id="registerPassword">
+		</div>
+		<div class="form-group col-md-6">
+			<label for="registerPassword1">Jelszó</label>
+			<input type="password" class="form-control" id="registerPassword1">
+		</div>
+	</div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Vissza</button>
-        <button type="button" class="btn btn-primary">Bejelentkezés</button>
+        <button type="submit" class="btn btn-primary" name="login">Bejelentkezés</button>
       </div>
+      </form>
     </div>
   </div>
 </div>
   </li>
   <li class="nav-item">
     
-    <a class="nav-link" href="">Regisztráció</a>
+    <a class="nav-link" href="index.php?P=register">Regisztráció</a>
 
   </li>
-  
+  <?php else : ?>
+    <?php endif;?>
   
 </ul>
 <div class="modal" tabindex="-1" role="dialog" id="myModal">
@@ -54,3 +67,4 @@
     </div>
   </div>
 </div>
+  
